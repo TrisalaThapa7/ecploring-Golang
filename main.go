d2615df3c7035ccbcda557511092a53682a6a3aa
TrisalaThapa7 <trisalathapa7@gmail.com>
2024-06-13 10:49:27 -0400

    Day -3 of learning Go, - Contionals and Boolean data types

--- main.go
+++ main.go
@@ -19,27 +19,46 @@ func main() {
 		var lastName string
 		var email string
 		var userTickets uint
+
 		fmt.Println("Enter your first name: ")
 		fmt.Scan(&firstName)
+
 		fmt.Println("Enter your last name: ")
 		fmt.Scan(&lastName)
+
 		fmt.Println("Enter your email: ")
 		fmt.Scan(&email)
+
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
+
+		if userTickets > remainingTickets {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets.\n", remainingTickets, userTickets)
+			continue
+		}
+
 		remainingTickets = remainingTickets - userTickets
 		bookings = append(bookings, firstName+" "+lastName)
+
 		fmt.Printf("The whole array: %v\n", bookings)
 		fmt.Printf("The first value: %v\n", bookings[0])
 		fmt.Printf("Slice type: %T \n", bookings)
 		fmt.Printf("Slice length: %v \n", len(bookings))
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v. \n", firstName, lastName, userTickets, email)
 		fmt.Printf("%v tickets remaining for %v conferenceTickets.\n", remainingTickets, conferenceTickets)
+
 		firstNames := []string{}
 		for _, booking := range bookings {
 			var names = strings.Fields(booking)
 			firstNames = append(firstNames, names[0])
 		}
 		fmt.Printf("The first names of bookings are: %v \n", firstNames)
+
+		if remainingTickets == 0 {
+
+			fmt.Println("Our conference is fully booked, please come next time!")
+			break
+		}
+
 	}
 }
